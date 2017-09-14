0e4ab64223023ca8d2881391d91c9e79c44ced55
Ömer Cinbat <ocinbat@outlook.com>
2017-09-14 15:44:10 +0300

    Added support to initialize multiple consumers for same bus instance.

--- MetroBus.ConsumerSample/FooCommandConsumerService.cs
+++ MetroBus.ConsumerSample/FooCommandConsumerService.cs
@@ -1,34 +1,38 @@
 ﻿using System.Configuration;
 using MassTransit;
+using MetroBus.ConsumerSample.Contracts;
 
 namespace MetroBus.ConsumerSample
 {
     //You can use this class with TopShelf as a windows service.
     public class FooCommandConsumerService
     {
-        private IBusControl _consumerBusControl;
-        private string _rabbitMqUri;
-        private string _rabbitMqUserName;
-        private string _rabbitMqPassword;
-        private string _queueName;
+        private readonly IBusControl _consumerBusControl;
 
         public FooCommandConsumerService()
         {
-            _rabbitMqUri = ConfigurationManager.AppSettings["RabbitMqUri"];
-            _rabbitMqUserName = ConfigurationManager.AppSettings["RabbitMqUserName"];
-            _rabbitMqPassword = ConfigurationManager.AppSettings["RabbitMqPassword"];
-            _queueName = ConfigurationManager.AppSettings["FooQueue"];
+            var rabbitMqUri = ConfigurationManager.AppSettings["RabbitMqUri"];
+            var rabbitMqUserName = ConfigurationManager.AppSettings["RabbitMqUserName"];
+            var rabbitMqPassword = ConfigurationManager.AppSettings["RabbitMqPassword"];
 
-            _consumerBusControl = MetroBusInitializer.Instance.UseRabbitMq(_rabbitMqUri, _rabbitMqUserName, _rabbitMqPassword)
-                             .InitializeConsumer<FooCommandConsumer>(_queueName).Build();
+            _consumerBusControl = MetroBusInitializer.Instance
+                                    .UseRabbitMq(rabbitMqUri, rabbitMqUserName, rabbitMqPassword)
+                                    .RegisterConsumer<FooCommandConsumer>("foo.create-queue")
+                                    .RegisterConsumer<FirstFooCreatedEventConsumer>("foo-created.first-action-queue")
+                                    .RegisterConsumer<SecondFooCreatedEventConsumer>("foo-created.second-action-queue")
+                                    .Build();
         }
 
-        public void Start()
+        public async void Start()
         {
+            await _consumerBusControl.Publish<IFooCreatedEvent>(new FooCreatedEvent
+            {
+                Id = 5
+            });
             _consumerBusControl.Start();
         }
 
-        public void Stop()
+        public async void Stop()
         {
             _consumerBusControl.Stop();
         }
---
+++ MetroBus.ConsumerSample/SecondFooCreatedEventConsumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using MetroBus.ConsumerSample.Contracts;
+
+namespace MetroBus.ConsumerSample
+{
+    public class SecondFooCreatedEventConsumer : IConsumer<IFooCreatedEvent>
+    {
+        public async Task Consume(ConsumeContext<IFooCreatedEvent> context)
+        {
+            throw new Exception("Whoaaa...... Booom...");
+            await Console.Out.WriteLineAsync($"Second consumer says: Foo:{context.Message.Id} created.");
+        }
+    }
+}
