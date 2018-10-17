e8b4e87f28e68d5e8a619dcef96568e4f9901c3b
gokgokalp <gok.gokalp@yahoo.com>
2018-10-17 10:17:54 +0300

    metatags added for packaging

--- src/MetroBus.Microsoft.Extensions.DependencyInjection/MetroBusExtensions.cs
+++ src/MetroBus.Microsoft.Extensions.DependencyInjection/MetroBusExtensions.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using GreenPipes;
+using MassTransit;
+using MassTransit.RabbitMqTransport;
 
 namespace MetroBus.Microsoft.Extensions.DependencyInjection
 {
     public static class MetroBusExtensions
     {
+        public static MetroBusInitializer RegisterConsumer(this MetroBusInitializer instance, string queueName, IServiceProvider serviceProvider)
+        {
+            Action<IRabbitMqBusFactoryConfigurator, IRabbitMqHost> action = (cfg, host) =>
+            {
+                if (queueName == null)
+                {
+                    cfg.ReceiveEndpoint(host, ConfigureReceiveEndpoint(instance, serviceProvider));
+                }
+                else
+                {
+                    cfg.ReceiveEndpoint(host, queueName, ConfigureReceiveEndpoint(instance, serviceProvider));
+                }
+            };
+
+            instance.MetroBusConfiguration.BeforeBuildActions.Add(action);
+
+            return instance;
+        }
+
         private static Action<IRabbitMqReceiveEndpointConfigurator> ConfigureReceiveEndpoint(MetroBusInitializer instance, IServiceProvider serviceProvider)
         {
             return _ =>
